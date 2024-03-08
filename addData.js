7990db004e598d0a2d7ce0f6fc7c62f8a2bfbd4f
isuchart <suchart2005@gmail.com>
2024-03-08 12:43:42 +0700

    Update addData.js

--- addData.js
+++ addData.js
@@ -24,26 +24,26 @@ function addDataToSheet() {
   let tabRoom = stdData.room;
   let scoreAvg = numDec(stdData.avgScore, 2);
 
-  var body = {
-    eight: {
-      date: myDate,
-      time: timeTest,
-      lineId: `${liff.getDecodedIDToken().sub}`,
-      lineName: `${liff.getDecodedIDToken().name}`,
-      room: `${stdData.room}`,
-      no: `${stdData.no}`,
-      stdid: `${stdData.stdid}`,
-      prename: `${stdData.prename}`,
-      fname: `${stdData.fname}`,
-      sname: `${stdData.sname}`,
-      subject: `${subjectNow}`,
-      // score: `${score}`,
-      scoreMax: `${stdData.maxScore}`,
-      scoreMin: `${stdData.minScore}`,
-      scoreAvg: numDec(scoreAvg, 2),
-      timeCount: `${stdData.timeCount}`,
-    },
-  };
+  // var body = {
+  //   eight: {
+  //     date: myDate,
+  //     time: timeTest,
+  //     lineId: `${liff.getDecodedIDToken().sub}`,
+  //     lineName: `${liff.getDecodedIDToken().name}`,
+  //     room: `${stdData.room}`,
+  //     no: `${stdData.no}`,
+  //     stdid: `${stdData.stdid}`,
+  //     prename: `${stdData.prename}`,
+  //     fname: `${stdData.fname}`,
+  //     sname: `${stdData.sname}`,
+  //     subject: `${subjectNow}`,
+  //     // score: `${score}`,
+  //     scoreMax: `${stdData.maxScore}`,
+  //     scoreMin: `${stdData.minScore}`,
+  //     scoreAvg: numDec(scoreAvg, 2),
+  //     timeCount: `${stdData.timeCount}`,
+  //   },
+  // };
 
   let url;
   switch (tabRoom) {
@@ -73,72 +73,53 @@ function addDataToSheet() {
             },
           };
 
-        fetch(url)
-          .then((response) => response.json())
-          .then((json) => {
-            // Do something with the data
-            json.eight.map((val) => {
-              if (val.stdid == stdData.stdid) {
-                // if(val.stdid=="88889"){
-                if (
-                  parseInt(val.scoreMax) < stdData.score ||
-                  parseInt(val.scoreMin) > stdData.score
-                ) {
-                  console.log(val.id, val.stdid, val.room, val.scoreMax,val.scoreMin,stdData.score);
-                  let url3 =
-                    "https://api.sheety.co/2fb80a9f78e03a86a9c645b99d92db92/score662M4Sheety/eight/"+val.id
-                    console.log(url3)
+        // fetch(url)
+        //   .then((response) => response.json())
+        //   .then((json) => {
+        //     // Do something with the data
+        //     json.eight.map((val) => {
+        //       if (val.stdid == stdData.stdid) {
+        //         // if(val.stdid=="88889"){
+        //         if (
+        //           parseInt(val.scoreMax) < stdData.score ||
+        //           parseInt(val.scoreMin) > stdData.score
+        //         ) {
+        //           console.log(val.id, val.stdid, val.room, val.scoreMax,val.scoreMin,stdData.score);
+        //           let url3 =
+        //             "https://api.sheety.co/2fb80a9f78e03a86a9c645b99d92db92/score662M4Sheety/eight/"+val.id
+        //             console.log(url3)
 
-                  fetch(url3, {
-                    method: "PUT",
-                    headers: {
-                      "Content-Type": "application/json",
-                    },
-                    // body: JSON.stringify(body),
-                    body: JSON.stringify({ eight: body }),
-                  })
-                    .then((response) => {
-                      if (!response.ok) {
-                        throw new Error("Network response was not ok");
-                      }
-                      return response.json();
-                    })
-                    .then((json) => {
-                      // Do something with object
-                      console.log(json.eight);
-                      // console.log(response.json());
-                    })
-                    .catch((error) => {
-                      console.error(
-                        "There was a problem with the fetch operation:",
-                        error
-                      );
-                    });
-                }
-              }
-              // else {
-              //   fetch(url, {
-              //     method: "POST",
-              //     headers: {
-              //       "Content-Type": "application/json", // เพิ่ม header สำหรับระบุประเภทข้อมูลเป็น JSON
-              //     },
-              //     body: JSON.stringify(body),
-              //   })
-              //     .then((response) => response.json())
-              //     .then((json) => {
-              //       // Do something with object
-              //       // console.log(json.datum); // เปลี่ยนเป็น json.datum แทน body.datum
-              //     })
-              //     .catch((error) => {
-              //       console.error("Error:", error);
-              //     });
-              //     break
-              // }
+        //           fetch(url3, {
+        //             method: "PUT",
+        //             headers: {
+        //               "Content-Type": "application/json",
+        //             },
+        //             // body: JSON.stringify(body),
+        //             body: JSON.stringify({ eight: body }),
+        //           })
+        //             .then((response) => {
+        //               if (!response.ok) {
+        //                 throw new Error("Network response was not ok");
+        //               }
+        //               return response.json();
+        //             })
+        //             .then((json) => {
+        //               // Do something with object
+        //               console.log(json.eight);
+        //               // console.log(response.json());
+        //             })
+        //             .catch((error) => {
+        //               console.error(
+        //                 "There was a problem with the fetch operation:",
+        //                 error
+        //               );
+        //             });
+        //         }
+        //       }
               
-              // return json.data
-            });
-            console.log(json.eight);
-          });
+        //     });
+        //     console.log(json.eight);
+        //   });
 
         // var body = {
         //   eight: {
@@ -161,6 +142,7 @@ function addDataToSheet() {
         //   },
         // };
       }
+      break;
 
     case "ม.4/10":
       {
